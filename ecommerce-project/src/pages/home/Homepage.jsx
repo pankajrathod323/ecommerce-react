49b8be9c774aa67bdd94e4cc4d4465b535220119
pankaj rathod <pankajrathod272004@gmail.com>
2026-02-17 11:53:44 +0530

    Convert home and checkout to async await

--- ecommerce-project/src/pages/home/Homepage.jsx
+++ ecommerce-project/src/pages/home/Homepage.jsx
@@ -9,9 +9,12 @@ export function HomePage({cart}) {
 
  
   useEffect( () => {
-     axios.get("/api/products").then((response) => {
-       setProducts(response.data);
-   })  
+     const getHomeData = async () => {
+         let response = await axios.get("/api/products");
+         setProducts(response.data);
+     }
+
+     getHomeData();
 }, [])
 
 
