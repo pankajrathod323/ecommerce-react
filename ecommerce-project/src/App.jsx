61f6b828fb1f7bff7c306ef45732ddbb891d763b
pankaj rathod <pankajrathod272004@gmail.com>
2026-02-25 23:57:04 +0530

    Create add to cart feature

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,28 +1,28 @@
 import './App.css'
 import axios from 'axios';
-import { useEffect, useState } from 'react';  
+import { useEffect, useState } from 'react';
 import { HomePage } from './pages/home/Homepage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
-import {Routes, Route} from 'react-router';
+import { Routes, Route } from 'react-router';
 import { OrdersPage } from './pages/order/OrdersPage';
 
 function App() {
 
   const [cart, setCart] = useState([]);
 
+  const loadCart = async () => {
+    let response = await axios.get("/api/cart-items?expand=product");
+    setCart(response.data);
+  }
   useEffect(() => {
-     const fetchAppData = async () => {
-        let response = await axios.get("/api/cart-items?expand=product");
-        setCart(response.data);
-     }
-       fetchAppData();
-  },[])
+     loadCart();
+  }, [])
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart}/>}/>
-      <Route path='checkout' element = {<CheckoutPage cart={cart}/>} />
-      <Route path='orders' element = {<OrdersPage cart={cart}/>} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} />} />
+      <Route path='orders' element={<OrdersPage cart={cart} />} />
     </Routes>
   )
 }
--- ecommerce-project/src/pages/home/Homepage.jsx
+++ ecommerce-project/src/pages/home/Homepage.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { ProductsGrid } from "./ProductsGrid";
 import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
-export function HomePage({cart}) {
+export function HomePage({cart, loadCart}) {
     
   const [products, setProducts] = useState([]);
 
@@ -23,7 +23,7 @@ export function HomePage({cart}) {
       <title>Ecommerce Project</title>
       <Header  cart= {cart} />
       <div className="home-page">
-        <ProductsGrid products={products}/>
+        <ProductsGrid products={products} loadCart={loadCart}/>
 
       </div>
     </>
